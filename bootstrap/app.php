e3e89caa54bbccac124abe0d383e7b15b2f77e41
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-19 07:43:02 +0700

    fix: penambahan/perbaikan pada bootstrap/app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'role' => \App\Http\Middleware\RoleMiddleware::class,
+            'session.timeout' => \App\Http\Middleware\SessionTimeoutMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
