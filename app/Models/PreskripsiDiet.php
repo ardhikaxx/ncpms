c4b23f3d065dd5520575d6d0f8ce9272945540b8
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-19 07:43:02 +0700

    fix: penambahan/perbaikan pada app/Models/PreskripsiDiet.php

--- app/Models/PreskripsiDiet.php
+++ app/Models/PreskripsiDiet.php
@@ -13,6 +13,9 @@ class PreskripsiDiet extends Model implements \OwenIt\Auditing\Contracts\Auditab
     protected $table = 'preskripsi_diets';
     protected $fillable = ['kunjungan_id', 'formula_basal', 'berat_badan_acuan', 'kebutuhan_energi_basal_kkal', 'faktor_aktivitas', 'faktor_stres', 'faktor_koreksi_usia', 'total_kebutuhan_energi_kkal', 'persen_karbohidrat', 'gram_karbohidrat', 'persen_protein', 'gram_protein', 'persen_lemak', 'gram_lemak', 'gram_serat', 'batas_natrium_mg', 'batas_kalium_mg', 'batas_fosfor_mg', 'batas_cairan_ml', 'bentuk_makanan', 'frekuensi_makan_utama', 'frekuensi_selingan', 'pantangan_spesifik', 'catatan_klinis', 'tujuan_terapi', 'target_luaran_klinis', 'tanggal_mulai', 'tanggal_evaluasi', 'durasi_hari', 'status', 'dibuat_oleh', 'disetujui_oleh', 'disetujui_pada', 'satusehat_careplan_id'];
     protected $casts = ['catatan_klinis' => 'encrypted', 'pantangan_spesifik' => 'array', 'target_luaran_klinis' => 'array', 'tanggal_mulai' => 'date', 'tanggal_evaluasi' => 'date', 'disetujui_pada' => 'datetime'];
-    
 
+    public function kunjungan() { return $this->belongsTo(Kunjungan::class); }
+    public function pembuat() { return $this->belongsTo(Pengguna::class, 'dibuat_oleh'); }
+    public function penyetuju() { return $this->belongsTo(Pengguna::class, 'disetujui_oleh'); }
+    public function detailMenuHarians() { return $this->hasMany(DetailMenuHarian::class); }
 }
