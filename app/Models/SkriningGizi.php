dbafc92bb84ab252744aa034f40394d385e9bd2b
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-20 00:13:28 +0700

    feat: penambahan kolom detail skrining untuk berbagai metode spesifik (STRONGkids, MNA, NRS-2002)

--- app/Models/SkriningGizi.php
+++ app/Models/SkriningGizi.php
@@ -11,7 +11,8 @@ class SkriningGizi extends Model implements \OwenIt\Auditing\Contracts\Auditable
     use HasFactory, \OwenIt\Auditing\Auditable;
 
     protected $table = 'skrining_gizis';
-    protected $fillable = ['kunjungan_id', 'metode_skrining', 'skor_penurunan_bb', 'skor_penurunan_asupan', 'skor_keparahan_penyakit', 'skor_usia', 'total_skor', 'kategori_risiko', 'rekomendasi_tindak_lanjut', 'dilakukan_oleh'];
+    protected $fillable = ['kunjungan_id', 'metode_skrining', 'skor_penurunan_bb', 'skor_penurunan_asupan', 'skor_keparahan_penyakit', 'skor_usia', 'total_skor', 'kategori_risiko', 'rekomendasi_tindak_lanjut', 'dilakukan_oleh', 'detail_skrining'];
+    protected $casts = ['detail_skrining' => 'array'];
 
     public function kunjungan() { return $this->belongsTo(Kunjungan::class); }
     public function pelaksana() { return $this->belongsTo(Pengguna::class, 'dilakukan_oleh'); }
