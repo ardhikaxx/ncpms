e06e30c97bedfbe174ffa8f8060b8da7fe2989c3
ardhikaxx <ardhikayanuar58@gmail.com>
2026-06-19 07:43:01 +0700

    fix: penambahan/perbaikan pada app/Models/PemeriksaanFisikGizi.php

--- app/Models/PemeriksaanFisikGizi.php
+++ app/Models/PemeriksaanFisikGizi.php
@@ -13,6 +13,6 @@ class PemeriksaanFisikGizi extends Model implements \OwenIt\Auditing\Contracts\A
     protected $table = 'pemeriksaan_fisik_gizis';
     protected $fillable = ['kunjungan_id', 'tekanan_darah_sistolik', 'tekanan_darah_diastolik', 'nadi_per_menit', 'respirasi_per_menit', 'suhu_celsius', 'saturasi_oksigen_persen', 'edema', 'lokasi_edema', 'tanda_defisiensi', 'gangguan_gastrointestinal', 'kondisi_mulut', 'kekuatan_genggam_kg', 'catatan_klinis', 'dicatat_oleh'];
     protected $casts = ['tanda_defisiensi' => 'array', 'gangguan_gastrointestinal' => 'array'];
-    
-
+    public function kunjungan() { return $this->belongsTo(Kunjungan::class); }
+    public function pencatat() { return $this->belongsTo(Pengguna::class, 'dicatat_oleh'); }
 }
